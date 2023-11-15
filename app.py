7184ea9eb1377a3ca0237c011177c4ef3578291c
Bryan Shea <122943518+Bryan-Webifi-dev@users.noreply.github.com>
2023-11-15 14:19:43 -0500

    navbar complete, added tags for main content

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-# @app.route("/", methods=["GET", "POST"])
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/stack")
+def stack():
+    # You'll add more logic here later for handling stack operations
+    return render_template("stack.html")
+
+@app.route("/queue")
+def queue():
+    # Similarly, add logic for queue operations
+    return render_template("queue.html")
+
+# Add additional routes for other data structures like linked lists, trees, graphs, etc.
 
 if __name__ == "__main__":
     app.run(debug=True)
