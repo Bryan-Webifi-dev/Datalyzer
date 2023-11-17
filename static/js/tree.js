230f231e18d9fddcd25066e66eef835e54dbde8f
Bryan Shea <122943518+Bryan-Webifi-dev@users.noreply.github.com>
2023-11-17 15:52:00 -0500

    Working data structure pages

--- static/js/tree.js
+++ static/js/tree.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Tree logic will be more complex and may require a more advanced approach
-    // This is a placeholder for basic functionality
+    const treeVisualization = document.getElementById("tree-visualization");
+    const addNodeButton = document.getElementById("add-node-btn");
+    const treeInput = document.getElementById("tree-input");
+
+    addNodeButton.addEventListener("click", function() {
+        const inputValue = treeInput.value.trim();
+        if (inputValue) {
+            const newNode = document.createElement("div");
+            newNode.classList.add("tree-node");
+            newNode.textContent = inputValue;
+            treeVisualization.appendChild(newNode);
+            treeInput.value = '';
+        }
+    });
+
+    // More complex logic will be needed for properly visualizing tree structure
 });
