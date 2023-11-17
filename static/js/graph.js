65c255678e57747798ceb47181a59e94586e2641
Bryan Shea <122943518+Bryan-Webifi-dev@users.noreply.github.com>
2023-11-17 18:04:23 -0500

    graphs

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Graph logic, including vertex and edge manipulation
-    // This might involve using a library or a more complex custom implementation
+    const graphVisualization = document.getElementById("graph-visualization");
+    const addNodeButton = document.getElementById("add-node-btn");
+    const graphInput = document.getElementById("graph-input");
+
+    addNodeButton.addEventListener("click", function() {
+        const inputValue = graphInput.value.trim();
+        if (inputValue) {
+            const newNode = document.createElement("div");
+            newNode.classList.add("graph-node");
+            newNode.textContent = inputValue;
+            graphVisualization.appendChild(newNode);
+            graphInput.value = '';
+        }
+    });
+
+    // Future enhancements: logic for adding edges and more complex graph operations
 });
